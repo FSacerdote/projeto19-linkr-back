ed3fb6f150d174f6b4741e0bbc3a4d382f9e7e95
Nathan4Andrade <nathanandrade20@gmail.com>
2023-08-15 11:50:18 -0300

    feat: folders structure

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import likesRouter from "./likes.routes.js";
 
 const router = Router();
+
+router.use(likesRouter);
 
 export default router;
---
+++ src/routes/likes.routes.js
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const likesRouter = Router();
+
+export default likesRouter;
