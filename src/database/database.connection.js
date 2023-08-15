f3f0af4d61a451484d27be90bb03d25fc5d564f5
Nathan4Andrade <nathanandrade20@gmail.com>
2023-08-15 10:55:50 -0300

    fix: semicolon

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -1,14 +1,14 @@
-import pg from "pg"
-import dotenv from "dotenv"
+import pg from "pg";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const { Pool } = pg
+const { Pool } = pg;
 
 const config = {
-    connectionString: process.env.DATABASE_URL,
-}
+  connectionString: process.env.DATABASE_URL,
+};
 
 if (process.env.NODE_ENV === "production") config.ssl = true;
 
-export const db = new Pool(config)
+export const db = new Pool(config);
