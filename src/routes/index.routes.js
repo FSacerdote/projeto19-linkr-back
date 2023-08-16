b5b75372cd027adaf0426f6d65e64225b23adca7
gpsign <gpsignorini@gmail.com>
2023-08-16 02:14:51 -0300

    teste

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import userRoute from "./user.routes.js";
+
 import searchRoute from "./search.routes.js";
+
 
 const router = Router();
 router.use(userRoute);
+
 router.use(searchRoute);
+
+
 export default router;
