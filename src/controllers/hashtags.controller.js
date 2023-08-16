516396f18a7d78e25bcc9f1eae97d7d64c55c09f
MarcusDantas <marvinsd@gmail.com>
2023-08-15 21:30:13 -0300

    feat: add /hashtags/:hashtag get endpoint to retrieve posts by hashtag

--- src/controllers/hashtags.controller.js
+++ src/controllers/hashtags.controller.js
@@ -1,14 +1,26 @@
 import {
-  getHashtagsByAmount
+  getHashtagsByFrequency,
+  getPostsByHashtag,
 } from "../repositories/hashtags.repository.js";
 
 export async function getTrending(req, res) {
-    const { limit } = req.query;
+  const { limit } = req.query;
 
-    try {
-        const trending = await getHashtagsByAmount(limit);
-        res.send(trending);
-    } catch (error) {
-        return res.status(500).send(error.message);
-    }
+  try {
+    const trending = await getHashtagsByFrequency(parseInt(limit));
+    res.send(trending);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+}
+
+export async function getHashtagPosts(req, res) {
+  const hashtag = req.params.hashtag;
+
+  try {
+    const posts = await getPostsByHashtag(hashtag);
+    res.send(posts);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
 }
--- src/repositories/hashtags.repository.js
+++ src/repositories/hashtags.repository.js
@@ -1,6 +1,6 @@
 import { db } from "../database/database.connection.js";
 
-export async function getHashtagsByAmount(limit) {
+export async function getHashtagsByFrequency(limit) {
   let query = `
     SELECT h.name, COUNT(*) AS frequency
     FROM hashtags h
@@ -18,4 +18,31 @@ export async function getHashtagsByAmount(limit) {
 
   const trending = await db.query(query, params);
   return trending.rows;
+}
+
+export async function getPostsByHashtag(hashtag) {
+  const posts = await db.query(
+    `SELECT
+      p.id,
+      p."userId",
+      u.username,
+      u."pictureUrl",
+      p.url,
+      p.description
+    FROM
+      posts p
+    JOIN
+      users u ON p."userId" = u.id
+    JOIN
+      "postHashtag" ph ON p.id = ph."postId"
+    JOIN
+      hashtags h ON ph."hashtagId" = h.id
+    WHERE
+      h.name = $1
+    ORDER BY
+     p.id DESC;`,
+    [hashtag]
+  );
+
+  return posts.rows;
 }
