f04662a6b8672986ed6937f5f8b4bc47fee42553
anapacker <anaamaria.packer@gmail.com>
2023-08-17 23:50:36 -0300

    Atualizações nas validações de Joi para login e cadastro

--- src/schemas/signupSchema.js
+++ src/schemas/signupSchema.js
@@ -1,9 +1,9 @@
 import Joi from "joi"
 
 export const signupSchema = Joi.object({
-  username: Joi.string().required(),
+  username: Joi.string().min(3).trim().required(),
   pictureUrl:Joi.string().uri().required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().required().min(3)
+  email: Joi.string().email().trim().min(5).required(),
+  password: Joi.string().min(6).required()
 
 })
--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -1,7 +1,6 @@
 import Joi from "joi";
 
 export const userSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().required()
-
+  email: Joi.string().email().trim().min(5).required(),
+  password: Joi.string().min(6).required()
 })
