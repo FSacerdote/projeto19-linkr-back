64215bcfaebbdaed4da4ac93e85822559711e743
Marcus Dantas <129130733+omarcusdantas@users.noreply.github.com>
2023-08-23 15:36:40 -0300

    feat: add endpoints to get new posts (#32)

--- src/controllers/search.controller.js
+++ src/controllers/search.controller.js
@@ -20,10 +20,10 @@ export async function getUsersList(req, res) {
 
 export async function getUserPosts(req, res) {
   const { id } = req.params;
-  const { offset, limit } = req.query;
+  const { offset, limit, untilId } = req.query;
 
   try {
-    const postsFound = await getPostsByUserId(id, offset, limit);
+    const postsFound = await getPostsByUserId(id, offset, limit, untilId);
     const postArr = [];
 
     for (const post of postsFound.rows) {
